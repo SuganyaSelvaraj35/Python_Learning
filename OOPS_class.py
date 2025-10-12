477e6a9039c85b8a2b773ce861a37a510ca980d8
Suganya Selvaraj <suganya.selvaraj35@gmail.com>
2025-10-12 18:46:47 +0530

    Add OOPS_class.py

---
+++ OOPS_class.py
@@ -0,0 +1,13 @@
+class Calculator:
+
+    a = 10
+    b = 20
+    Result = a + b
+
+    def Addition(self):
+        print('The result of two numbers')
+
+obj = Calculator()
+obj.Addition()
+print(obj.Result)
+
