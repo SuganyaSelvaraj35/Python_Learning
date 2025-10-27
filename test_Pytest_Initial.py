9833090a9ad94637e67c1300777bbaa5c5c81f94
Suganya Selvaraj <suganya.selvaraj35@gmail.com>
2025-10-28 05:02:56 +0530

    String practice

---
+++ test_Pytest_Initial.py
@@ -0,0 +1,7 @@
+
+def test_initial_test(prework):
+    print("Login successful")
+
+def test_second_test(postwork):
+    print("document created!")
+    assert postwork == "END"
---
+++ test_Pytest_Second.py
@@ -0,0 +1,6 @@
+
+def test_third_test(postwork):
+    print("Logout Page")
+
+def test_fourth_test(postwork):
+    print("closing the page")
