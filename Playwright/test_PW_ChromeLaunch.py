0cfc7bba8027b109e6da6efe7d35ce80a0b5e85f
Suganya Selvaraj <suganya.selvaraj35@gmail.com>
2025-12-10 04:58:04 +0530

    String practice

--- Playwright/test_PW_ChromeLaunch.py
+++ Playwright/test_PW_ChromeLaunch.py
@@ -1,6 +1,20 @@
+# For a customized requirement we can use this format
+
+import time
+from playwright.sync_api import Playwright
+from playwright.sync_api import Page
+
 
 def test_chromelaunch(playwright):
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
+    context = browser.new_context(no_viewport=True) # Maximize the chrome
     page = context.new_page()
-    page.goto("https://playwright.com/")
+    page.goto("https://practice.expandtesting.com/?utm_source=chatgpt.com")
+    Page.get_by_role("link", name="Test Login Page").click()
+
+# When the request is on single browser, headless=true(it will show the chrome launch), one context
+
+def test_secondway(page: Page):
+    page.goto("https://practice.expandtesting.com/?utm_source=chatgpt.com")
+    page.get_by_role("link", name = "Test Login Page").click()
+    time.sleep(3)
